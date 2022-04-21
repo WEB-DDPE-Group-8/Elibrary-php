7433f83af0e30ae923436b080f2cba9a0adf2aa7
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2022-04-21 11:13:38 +0300

    look and feel

--- inc/nav_bar.php
+++ inc/nav_bar.php
@@ -147,13 +147,13 @@ class="fas fa-shopping-cart"> <br>
       </form>
     </div>
 
-    <!-- register form -->
+    <!-- register form
     <div class="registration-form-container">
       <div id="close-login-btn" class="fas fa-times"></div>
 <!--
       <form action="./lib/db.php" method="POST" >
       -->
-      <form action="">
+      <!-- <form action="">
         <h3>sign in</h3>
         <span>username</span>
         <input
@@ -179,7 +179,7 @@ class="fas fa-shopping-cart"> <br>
         <p>forget password ? <a href="#">click here</a></p>
         <p>don't have an account ? <a href="regadmin.php">create one</a></p>
       </form>
-    </div> 
+    </div>  --> 
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
